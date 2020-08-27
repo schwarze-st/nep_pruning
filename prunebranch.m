5d8a00a7e2451b58a6b1ad112b1beff8464226f3
Stefan Schwarze <unegj@student.kit.edu>
2020-08-27 10:49:12 +0200

    Added missing set for x^nu_i being on bound

--- prunebranch.m
+++ prunebranch.m
@@ -9,9 +9,13 @@ for nu=1:N
        if flagi
            C = {};
            for p=1:length(B)
-               % Einfügen: wenn xnui auf lower bound ist, dann...
                A = B{p}{1,nu};
                b = B{p}{2,nu};
+               bnds1 = B{p}{3,nu};
+               bnds2 = B{p}{3,nu};
+               bnds2(i,1) = bnds2(i,1)+1;
+               bnds1(i,2) = bnds1(i,1);
+               C = {C,A,b,bnds1};
                J = find(A(:,i)<0);
                for j=1:size(J,1)
                    A = [A;-A(J(j),:)];
@@ -20,7 +24,7 @@ for nu=1:N
                        A = [A;A(J(k),:)];
                        b = [b;ceil(b(J(j),1)-abs(A(J(j),i)))+1];
                    end
-                   C = {C,{A;b;B{p}{3,nu}}};
+                   C = {C,{A;b;bnds2}};
                end
            end
            B=C;
@@ -28,7 +32,11 @@ for nu=1:N
        if flagii
            C = {};
            for p=1:length(B)
-               % Einfügen, wenn xnui auf upper bound ist, dann...
+               bnds1 = B{p}{3,nu};
+               bnds2 = B{p}{3,nu};
+               bnds2(i,2) = bnds2(i,2)-1;
+               bnds1(i,1) = bnds1(i,2);
+               C = {C,A,b,bnds1};
                A = B{p}{1,nu};
                b = B{p}{2,nu};
                J = find(A(:,i)>0);
@@ -39,7 +47,7 @@ for nu=1:N
                        A = [A;A(J(k),:)];
                        b = [b;ceil(b(J(j),1)-abs(A(J(j),i)))+1];
                    end
-                   C = {C,{A;b;B{p}{3,nu}}};
+                   C = {C,{A;b;bnds2}};
                end  
            end
            B=C;          
