65f4a32a0b8a7f7bcc82933d5622e5942088a00b
Stefan <unegj@student.kit.edu>
2020-10-14 11:12:12 +0200

    calculate last 4 instances

--- testwholealgorithm.m
+++ testwholealgorithm.m
@@ -1,18 +1,21 @@
-global EQ O T N_ITER P_REQ G_CALLS G_TIME N_I;
+% global EQ O T N_ITER P_REQ G_CALLS G_TIME N_I;
 
 % read instances
-S = dir('IntegerPrograms/TestSet2/*.mat');
-Names = {S.name};
-n_inst = size(Names,2);
-EQ = zeros(n_inst,1);
-O = zeros(n_inst,3);
-T = zeros(n_inst,3);
-N_ITER = zeros(n_inst,1);
-G_CALLS = zeros(n_inst,4);
-G_TIME = zeros(n_inst,4);
+% S = dir('IntegerPrograms/TestSet1/*.mat');
+% Names = {S.name};
+% n_inst = size(Names,2);
+% EQ = zeros(n_inst,1);
+% O = zeros(n_inst,3);
+% T = zeros(n_inst,3);
+% N_ITER = zeros(n_inst,1);
+% G_CALLS = zeros(n_inst,4);
+% G_TIME = zeros(n_inst,4);
 
-for i=1:n_inst
-    N_I=i
-    load(append('IntegerPrograms/TestSet2/',Names{i}));
+global N_I
+
+for i=27:n_inst
+    N_I=i;
+    name = append('IntegerPrograms/TestSet2/',Names{i});
+    load(name);
     branchingmethod(Omega,Gf);
 end
